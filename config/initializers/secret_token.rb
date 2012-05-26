d68a756849c3641052ed714f694e3294aec4b911
Sathish <sathish316@gmail.com>
2012-05-26 21:40:54 +0530

    Generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonifyRailsExample::Application.config.secret_token = 'f380dbb05ab4419841044f0a9ce6972e70a776bea639c598e6a6447104f28fd2a461f11952942cbeb717f9adfb16b83c2cd29424ab9cde7581c4f5025df23c8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JsonifyRailsExample::Application.config.session_store :cookie_store, key: '_jsonify_rails_example_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JsonifyRailsExample::Application.config.session_store :active_record_store
